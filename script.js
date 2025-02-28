0e92a8d02f338e202522e4c0db7507340c1cbba4
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-02-28 19:36:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function volume_sphere() {
-    //Write your code here
-  
+	let r = document.getElementById("radius").value;
+	
+	if(isNaN(r) || r < 0){
+		 document.getElementById("volume").value = "NaN";
+	}
+	else{
+		let v = (4/3) * Math.PI * Math.pow(r, 3);
+		document.getElementById("volume").value = v.toFixed(4);
+	}
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
